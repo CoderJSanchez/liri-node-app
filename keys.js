1acf81ca749d3a55cb08928939f9bfb99aace050
Julio Sanchez <jsanchez191980802@gmail.com>
2018-10-25 04:47:55 -0500

    got omdb to work

---
+++ keys.js
@@ -0,0 +1,8 @@
+//KEY 6f1be632
+//sample: http://www.omdbapi.com/?i=tt3896198&apikey=6f1be632
+
+console.log('this is loaded');
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
---
+++ liri.js
@@ -0,0 +1,41 @@
+var env = require("dotenv").config();
+var fs = require('fs');
+var moment = require('moment');
+var keysFile = require('./keys');
+var spot = require('node-spotify-api');
+var request =  require('request');
+var proc = process.argv;
+var userCommand = process.argv[2];
+var userChoise = process.argv[3];
+//var spotify = new Spotify(keys.spotify);
+
+switch(userCommand){
+    case 'movie-this':
+        movie();
+        break;
+    case 'consert-this':
+        consert();
+        break;
+    case 'spotify-this-song':
+        spot();
+        break;
+    case 'do-what-it-says':
+        doIt();
+        break;
+}
+function movie(){
+
+    request('http://www.omdbapi.com/?t=' + userChoise + '&apikey=6f1be632', function(err, response, body){
+    console.log('error:', err); 
+    console.log('statusCode:', response && response.statusCode); 
+    //console.log('body:', JSON.parse(body));
+    console.log( JSON.parse(body).Title);
+    console.log( JSON.parse(body).Year);
+    console.log( JSON.parse(body).imdbRating);
+    console.log( JSON.parse(body).Ratings[1].Source);
+    console.log( JSON.parse(body).Country);
+    console.log( JSON.parse(body).Language);
+    console.log( JSON.parse(body).Plot);
+    console.log( JSON.parse(body).Actors);
+    });
+}
